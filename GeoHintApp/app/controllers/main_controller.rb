1eefc055b536b70ae9d6eba25848dfd368b7e48a
Oana <oana@oana-K54L.oana.stroie@cti.pub.ro>
2014-12-13 17:59:13 +0200

    --

--- GeoHintApp/app/controllers/main_controller.rb
+++ GeoHintApp/app/controllers/main_controller.rb
@@ -26,12 +26,35 @@ class MainController < ActionController::Base
       @user_all = User.all
       @user = User.find_by(:id => params[:id])
       level = Level.find_by(:id => @user.level_id)
+      @hints = level.hints
       session[:level_id] = level.id
+      session[:user_id] = params[:id]
    end
    
    def game_page_json
       level = Level.find_by(:id => session[:level_id])
       render :json => level.hints
+   end
+   
+   def submit_answers
+      @hints = Level.find_by(:id => session[:level_id]).hints
+     # failed = 0
+      
+  #    @hints.each do |hint|
+   #      if( hint.answer != params[#{hint.id}] )
+      #        failed = 1
+    #     end
+    #  end
+      
+     # if( failed == 0 )
+           #next level
+     # end
+      puts "HELOO2"
+      puts params[1]
+      puts params[2]
+      puts "HELOO2"
+      id = session[:user_id]
+      redirect_to ("/main/#{id}")
    end
 
 end
--- GeoHintApp/config/routes.rb
+++ GeoHintApp/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   post '/welcome/signin' => 'main#signin'
   get '/main/:id' => 'main#game_page'
   get '/game_page_json' => 'main#game_page_json'
+  post '/submit_answers' => 'main#submit_answers'
 end
