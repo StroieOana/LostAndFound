8f404182cc8de0801aeb966ee67a66c34a41da33
Oana <oana@oana-K54L.oana.stroie@cti.pub.ro>
2014-12-14 15:12:47 +0200

    almost final app

--- GeoHintApp/config/routes.rb
+++ GeoHintApp/config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   post '/submit_answers' => 'main#submit_answers'
   get '/ranking' => 'main#ranking'
   post '/back' => 'main#back'
+  get '/addlevel' => 'main#addlevel'
+  post '/insertlevel' => 'main#insertlevel'
 end
